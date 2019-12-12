8a23a0744fc443e559d4a359526cc2411f361871
mohan tupakula <tmskaushik@gmail.com>
2019-12-12 20:02:33 +0900

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  
+  def ask
+   
+  end
+  def answer
+    @question = params[:question]
+    (@answer = "") if @question == "I am going to work right now!"
+    (@answer = "Silly question, get dressed and go to work!") if @question[(@question.length - 1)] == "?"
+  
+    @answer = "I don't care, get dressed and go to work!"
+  end
+end
